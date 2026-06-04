db750f83cdd0839320ef15abf8f5f125557fa57a
Manali Divase <vsharma0811gmail@onmicrosoft.com>
2026-06-04 18:14:31 +0530

    diff rg

--- main.tf
+++ main.tf
@@ -3,3 +3,8 @@ resource "azurerm_resource_group" "machine1" {
     location = "central india"
   
 }
+
+resource "azurerm_resource_group" "machine1" {
+    name = "machine1"
+    location = "east us"
+}
