32487dac186e9135ba93afb0d36b22d2c78445cb
Manali Divase <vsharma0811gmail@onmicrosoft.com>
2026-06-04 18:22:13 +0530

    rg created

--- main.tf
+++ main.tf
@@ -3,3 +3,15 @@ resource "azurerm_resource_group" "machine1" {
     location = "central india"
   
 }
+
+resource "azurerm_resource_group" "machine1" {
+    name = "machine1"
+    location = "east us"
+  
+}
+
+resource "azurerm_resource_group" "machine1" {
+    name = "machine2"
+    location = "east us"
+  
+}
